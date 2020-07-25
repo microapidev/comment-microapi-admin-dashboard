cf9deefc29ffdc747a1cb8bddf4ca1c308081bd9
simply-alliv <45206423+simply-alliv@users.noreply.github.com>
2020-07-25 22:50:50 +0000

    Deploying to gh-pages from  @ 8ff4d78c7fe7a4642ca7da9467a0e49ef252be47 🚀

---
+++ precache-manifest.4ab8f4b353b8b05bcf81c405d91d32d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cb1c047c466709f7038865069c6b06",
+    "url": "/comment-microapi-admin-dashboard/index.html"
+  },
+  {
+    "revision": "6cd293b4c29800ef260c",
+    "url": "/comment-microapi-admin-dashboard/static/js/2.ab38ae61.chunk.js"
+  },
+  {
+    "revision": "f07796f4f30300629a9fa688e41af979",
+    "url": "/comment-microapi-admin-dashboard/static/js/2.ab38ae61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9539231efd43ceb28b8",
+    "url": "/comment-microapi-admin-dashboard/static/js/main.3b125d20.chunk.js"
+  },
+  {
+    "revision": "87269b1f1f7651fa0b09",
+    "url": "/comment-microapi-admin-dashboard/static/js/runtime-main.40fe0b3d.js"
+  }
+]);
