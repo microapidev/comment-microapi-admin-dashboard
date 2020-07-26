403f69c3dee3900cb73e13d311da62dd13891334
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-26 12:06:42 +0200

    feat: update login tests

--- cypress/integration/login/login.e2e.js
+++ cypress/integration/login/login.e2e.js
@@ -5,20 +5,36 @@ describe("Login Page", () => {
     cy.visit("/");
   });
 
-  it("should redirect to the login page", () => {
-    cy.url().should("include", "#/login");
+  describe("Queries", () => {
+    it("should redirect to the login page", () => {
+      cy.url().should("include", "#/login");
+    });
+
+    it("should focus on username input", () => {
+      cy.get("#username").should("be.focused");
+    });
+
+    it("should require username and password", () => {
+      cy.get("#username-helper-text").should("not.exist");
+      cy.get("#password-helper-text").should("not.exist");
+      cy.get("#username").blur();
+      cy.get("button[type=submit]").click();
+      cy.get("#username-helper-text").should("contain.text", "Required");
+      cy.get("#password-helper-text").should("contain.text", "Required");
+    });
   });
 
-  it("should focus on username input", () => {
-    cy.get("#username").should("be.focused");
-  });
+  describe("Actions", () => {
+    it("should have the inputted username value", () => {
+      cy.get("#username")
+        .type("fake@email.com")
+        .should("have.value", "fake@email.com");
+    });
 
-  it("should require username and password", () => {
-    cy.get("#username-helper-text").should("not.exist");
-    cy.get("#password-helper-text").should("not.exist");
-    cy.get("#username").blur();
-    cy.get("button[type=submit]").click();
-    cy.get("#username-helper-text").should("contain.text", "Required");
-    cy.get("#password-helper-text").should("contain.text", "Required");
+    it("should have the inputted password value", () => {
+      cy.get("#username")
+        .type("fakepassword")
+        .should("have.value", "fakepassword");
+    });
   });
 });
